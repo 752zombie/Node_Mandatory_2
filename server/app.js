9d7d131cd35d785ab50c1f064bf9139daf3f199c
Magnus <752zombie@gmail.com>
2022-04-08 18:26:01 +0200

    some changes

--- server/app.js
+++ server/app.js
@@ -1,5 +1,10 @@
-const express = require("express");
+import express from "express";
 const app = express();
+
+app.use(express.json());
+
+import path from "path";
+app.use(express.static(path.resolve('../client/public')));
 
 app.listen(8080, () => {
     console.log("Server is running on port 8080");
