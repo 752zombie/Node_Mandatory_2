b9a72c6ba6b4e9a7406acee1b19b70b7a153ccb8
Magnus <752zombie@gmail.com>
2022-04-10 15:03:09 +0200

    better styling of navbar

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,36 +1,75 @@
 <script>
 	import { Router, Link, Route } from "svelte-navigator";
 	import Test from "./routes/Test.svelte";
+	import Home from "./routes/Home.svelte";
 </script>
 
 <Router>
-	<span class="link-wrapper">
-		<Link to="test" class="link">Go to /test</Link>
-	</span>
+	<nav class="link-wrapper">
+		<Link to="/" class="header-link space-between first-link">Home</Link>
+		<Link to="test" class="header-link space-between">Products</Link>
+		<Link to="test" class="header-link space-between">Products</Link>
+		<Link to="test" class="header-link align-left">Products</Link>
+		<Link to="login" class="header-link align-right">Login</Link>
+	</nav>
 	
 	<Route path="test" component={Test} />
+	<Route path="/" component={Home} />
 </Router>
 
-<h1>Hello</h1>
 
 <style>
+	nav {
+		display: flex;
+		justify-content: center;
+	}
+
+	nav :global(.align-right) {
+		margin-left: auto;
+		margin-right: 20px;
+	}
+
+	nav :global(.align-left) {
+		margin-right: auto;
+	}
+
+	nav :global(.space-between) {
+		margin-right: 20px;
+	}
+
+	nav :global(.first-link) {
+		margin-left: 10px;
+	}
 	
-	.link-wrapper :global(.link) {
-        color: red;
-    }
+	nav {
+		background-color: green;
+		padding-top: 25px;
+		padding-bottom: 25px;
+	}
+
+
+
+	nav :global(.header-link) {
+		text-decoration: none;
+		color: white;
+		font-size: x-large;
+	}
+
+	nav :global(.header-link:hover) {
+		text-decoration: underline;
+	}
+
+	:global(body) {
+		margin: 0px;
+		padding: 0px;
+	}
+	
 
 	main {
 		text-align: center;
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
 	}
 
 	@media (min-width: 640px) {
